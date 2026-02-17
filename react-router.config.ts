d3a4db1051c8aaf57e3342b904179244de0d97cf
vishalbabu87 <vishalbabu87@gmail.com>
2026-02-17 14:23:49 +0530

    Fix Vercel route recursion

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,7 @@
-import type { Config } from '@react-router/dev/config';
+import type { Config } from "@react-router/dev/config";
 
 export default {
-	appDirectory: './src/app',
-	ssr: true,
+  appDirectory: "./src/app",
+  ssr: true,
+  routeDiscovery: { mode: "initial" },
 } satisfies Config;
